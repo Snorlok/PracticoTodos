eede4b3f840ceb552e611910742909915aa1c7b6
Snorlok <sambucettia@gmail.com>
2025-08-15 12:23:13 -0300

    Add files via upload

---
+++ PracticoTodos/src/main/java/com/mycompany/practicotodos/PracticoTodos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicotodos;
+
+/**
+ *
+ * @author 54568747
+ */
+public class PracticoTodos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
