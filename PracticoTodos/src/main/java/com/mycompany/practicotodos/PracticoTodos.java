a7841f6e6c123c7ba8d9dc40c24fcf28496b203f
Snorlok <sambucettia@gmail.com>
2025-08-15 12:52:44 -0300

    puras barbaridades y estupideces

--- PracticoTodos/src/main/java/com/mycompany/practicotodos/PracticoTodos.java
+++ PracticoTodos/src/main/java/com/mycompany/practicotodos/PracticoTodos.java
@@ -11,6 +11,11 @@ package com.mycompany.practicotodos;
 public class PracticoTodos {
 
     public static void main(String[] args) {
-        System.out.println("Putos todos!");             
+        System.out.println("Putos todos!");
+        System.out.println("SEXOOOOOOOOOOOOOOOOOO");
+        System.out.println("pititopotitopereti");
+        System.out.println("meh");
+        double pi = 3.14;
+        System.out.println(pi + "to");
     }
 }
